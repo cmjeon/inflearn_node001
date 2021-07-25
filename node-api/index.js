70e6fb82c7f5d776ca1642fbc943c5d8346a9a61
cmj <chmin82@gmail.com>
2021-07-25 16:45:17 +0900

    `섹션3. 익스프레스 기초 - 에러 미들웨어` 까지

--- node-api/index.js
+++ node-api/index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-function logger(req, res, next) {
-  console.log('I am logger');
+function commonmw(req, res, next) {
+  console.log('commonmw');
+  next(new Error('error ouccered'));
+}
+
+function errormw(err, req, res, next) {
+  console.log(err.message);
   next();
 }
 
-function logger2(req, res, next) {
-  console.log('I am logger2');
-  next();
-}
-
-app.use(logger);
-app.use(logger2);
+app.use(commonmw);
+app.use(errormw);
 app.use(morgan('dev'));
 
 app.listen(3000, function() {
