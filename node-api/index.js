f80799b9167612989d3756cea285076f539fff33
cmj <chmin82@gmail.com>
2021-07-25 16:42:04 +0900

    `섹션3. 익스프레스 기초 - 다른 개발자가 만든 미들웨어 사용하기` 까지

---
+++ node-api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+function logger(req, res, next) {
+  console.log('I am logger');
+  next();
+}
+
+function logger2(req, res, next) {
+  console.log('I am logger2');
+  next();
+}
+
+app.use(logger);
+app.use(logger2);
+app.use(morgan('dev'));
+
+app.listen(3000, function() {
+  console.log('Server is running');
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+https://www.inflearn.com/course/%ED%85%8C%EC%8A%A4%ED%8A%B8%EC%A3%BC%EB%8F%84%EA%B0%9C%EB%B0%9C-tdd-nodejs-api/dashboard 강좌 실습
+
+실행하기
+
+```bash
+$ npm start
+# localhost:3000
+```
+
+테스트하기
+
+```bash
+$ npm test
+```
