0ff2b849b3536d772845203e02ced2c1b76d6a3d
cmj <chmin82@gmail.com>
2021-07-31 23:32:18 +0900

    `TDD 로 하는 API 서버 개발 - 사용자 조회 API 실패시`까지

--- node-api/index.js
+++ node-api/index.js
@@ -3,19 +3,37 @@ var morgan = require('morgan');
 var app = express();
 
 var users = [
-  {id: 1, name: 'alice'},
-  {id: 2, name: 'beck'},
-  {id: 3, name: 'chris'},
+  { id: 1, name: 'alice' },
+  { id: 2, name: 'beck' },
+  { id: 3, name: 'chris' },
 ];
 
 app.use(morgan('dev'));
 
-app.get('/users', function(req, res) {
-  res.json(users);
+app.get('/users', function (req, res) {
+  const limit = parseInt(req.query.limit || 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  return res.json(users.slice(0, limit));
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
+  const user = users.filter((user) => {
+    return user.id == id;
+  })[0];
+  if (!user) {
+    return res.status(400).end();
+  }
+  return res.json(user);
 })
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
 
 module.exports = app;
--- node-api/index.spec.js
+++ node-api/index.spec.js
@@ -2,7 +2,7 @@ const app = require('./index.js');
 const request = require('supertest');
 const should = require('should');
 
-describe('GET /users는', () => {
+describe('GET /users', () => {
   describe('성공시', () => {
     it('유저 객체를 담은 배열로 응답한다', (done) => { // done
       request(app)
@@ -10,8 +10,49 @@ describe('GET /users는', () => {
         .end((err, res) => {
           res.body.should.be.instanceOf(Array);
           done();
-        })
-    })
-  })
-
+        });
+    });
+    it('최대 limit 갯수만큼 응답한다', (done) => {
+      request(app)
+        .get('/users?limit=2')
+        .end((err, res) => {
+          res.body.should.be.lengthOf(2);
+          done();
+        });
+    });
+  });
+  describe('실패시', () => {
+    it('limit 이 숫자형이 아니면 400을 응답한다', (done) => {
+      request(app)
+        .get('/users?limit=two')
+        .expect(400)
+        .end(done);
+    });
+  });
+});
+describe('GET /users/:id', () => {
+  describe('성공시', () => {
+    it('id가 1인 유저 객체를 반환한다', (done) => {
+      request(app)
+        .get('/users/1')
+        .end((err, res) => {
+          res.body.should.have.property('id', 1);
+          done();
+        });
+    });
+  });
+  describe('실패시', () => {
+    it('id가 숫자가 아닐 경우 400으로 응답한다', (done) => {
+      request(app)
+        .get('/users/one')
+        .expect(400)
+        .end(done);
+    });
+  });
+  it('id로 유저를 찾을 수 없을 경우 404으로 응답한다', (done) => {
+    request(app)
+      .get('/users/999')
+      .expect(400)
+      .end(done);
+  });
 })
